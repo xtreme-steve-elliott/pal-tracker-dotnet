5ce8346faee8f1b0f13ffae551facfa873c6371d
Steve E <selliott@pivotal.io>
2018-04-10 16:26:04 -0400

    Initial commit for PAL

---
+++ src/PalTracker/WelcomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    [Route("/")]
+    public class WelcomeController : Controller
+    {
+        [HttpGet]
+        public string SayHello() => "hello";
+    }
+}
